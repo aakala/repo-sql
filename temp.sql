f20e321ba93062031add7534ceac8094083a6f6a
aishwarya kala <aishwaryakala@gmail.com>
2022-04-16 09:07:27 -0400

    Add overall usage SQL
    Add overall usage SQL

--- temp.sql
+++ temp.sql
@@ -27,3 +27,15 @@ AND    a.inst_id=b.inst_id
 AND    a.inst_id=p.inst_id
 ORDER BY b.tablespace, b.blocks desc
 /
+
+SELECT A.tablespace_name tablespace, D.mb_total,
+    SUM (A.used_blocks * D.block_size) / 1024 / 1024 mb_used,
+    D.mb_total - SUM (A.used_blocks * D.block_size) / 1024 / 1024 mb_free
+   FROM v$sort_segment A,
+    (
+   SELECT B.name, C.block_size, SUM (C.bytes) / 1024 / 1024 mb_total
+    FROM v$tablespace B, v$tempfile C
+     WHERE B.ts#= C.ts#
+      GROUP BY B.name, C.block_size) D
+    WHERE A.tablespace_name = D.name
+    GROUP by A.tablespace_name, D.mb_total;
